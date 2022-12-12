83c20bb6ce5076e0f374cfafcc6f335d9c4d1043
Guru Nanma P <42802604+18nanma@users.noreply.github.com>
2022-12-12 11:37:39 -0600

    queue css changes

--- src/Components/Prices.js
+++ src/Components/Prices.js
@@ -8,7 +8,21 @@ function Prices(){
     <div className='radio'>
     <div class="grid-container">
   <div class="grid-item"><h1>BOWLING</h1></div>
+  <div class="grid-item">
+    <p> U of I Studetns $3.75</p>
+    <p> U of I Affiliates $4.50</p>
+    <p> General Public $5.25</p>
+    <p> U of I Studetns $3.75</p>
+  </div>
+  <div class="grid-item"><p>For Students and Affiliates only</p></div>
   <div class="grid-item"><h1>BILLIARDS</h1></div>
+  <div class="grid-item">
+    <p> U of I Studetns $3.75</p>
+    <p> U of I Affiliates $4.50</p>
+    <p> General Public $5.25</p>
+    <p> U of I Studetns $3.75</p>
+  </div>
+  <div class="grid-item"><p>For Students and Affiliates only</p></div>
   <div class="grid-item"><h1>DRINKS</h1></div>  
   <div class="grid-item">
     <p> U of I Studetns $3.75</p>
@@ -16,18 +30,8 @@ function Prices(){
     <p> General Public $5.25</p>
     <p> U of I Studetns $3.75</p>
   </div>
-  <div class="grid-item"> <p> U of I Studetns $3.75</p>
-    <p> U of I Affiliates $4.50</p>
-    <p> General Public $5.25</p>
-    <p> U of I Studetns $3.75</p></div>
-  <div class="grid-item">
-  <p> U of I Studetns $3.75</p>
-    <p> U of I Affiliates $4.50</p>
-    <p> General Public $5.25</p>
-    <p> U of I Studetns $3.75</p></div> 
-    <div class="grid-item"><p>For Students and Affiliates only</p></div>
-  <div class="grid-item"><p>For Students and Affiliates only</p></div>
-  <div class="grid-item"><p>Pre tax</p></div>   
+  
+    <div class="grid-item"><p>Pre tax</p></div>   
   
 </div>
           </div>
@@ -39,6 +43,3 @@ function Prices(){
   
 
   export default Prices
-
-
-
--- src/Components/RegisterPageTable.jsx
+++ src/Components/RegisterPageTable.jsx
@@ -29,10 +29,10 @@ function RegisterPageTable() {
         <div class="registerPageTableBody">
             <tbody>
             <tr>
-                <td className="registerPageTable"><span>Bowling</span>: {data.bowling} in queue</td>
+                <td className="registerPageTable wrapper"><h6>Bowling: {data.bowling} in queue</h6></td>
             </tr>
             <tr>
-                <td class="registerPageTable"><span>Billiards</span>: {data.billiards} in queue</td>
+                <td class="registerPageTable wrapper"><h6>Billiards: {data.billiards} in queue</h6></td>
             </tr>
             </tbody>
         </div>
